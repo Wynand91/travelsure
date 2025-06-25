1ad3eef7563135ee8687de5979f25f544b78e699
Wynand Theron <wtheron2@yahoo.com>
2025-06-25 11:26:55 +0200

    add common localhost urls for CORS policy

--- src/project/settings.py
+++ src/project/settings.py
@@ -98,7 +98,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # CORS config
 CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS', False)
-CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', [])
+COMMON_LOCALHOST_URLS = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:8001",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:8001",
+]
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', COMMON_LOCALHOST_URLS)
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'authorization',
 ]
